53209fed6a519e34c200662f184780c12d892e2d
yeung-github <kelvin.y@hotmail.com>
2024-03-16 13:22:39 +0800

    PI controller

---
+++ entry/src/main/ets/pages/BaseDemo/RaspberryPi/api/moveDriver.ts
@@ -0,0 +1,24 @@
+import { service as http } from '../../../Utils/httpUtils';
+
+const baseURL = "http://192.168.149.1:9030";
+const path = baseURL;
+
+interface RPCData{
+  method:string;
+  params?:any;
+}
+
+class MoveDriver {
+  movement(data:RPCData){
+    const dataFormat = {...data,jsonrpc:"2.0",id:1}
+    return http(
+      `${path}`,
+      {
+        method:"POST",
+        data:dataFormat
+      }
+    );
+  }
+}
+const moveAPI = new MoveDriver()
+export { moveAPI } ;
